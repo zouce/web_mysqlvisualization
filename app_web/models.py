108225b9ce6430ebe7d9c67d867f444bb2a86b56
zouce <zoucece@sina.com>
2023-01-01 00:09:02 +0800

    del db update query

--- app_web/models.py
+++ app_web/models.py
@@ -5,3 +5,8 @@ class Testtable(models.Model):
     col2 = models.CharField(max_length=256, blank=True)
     col3 = models.CharField(max_length=256, blank=True)
     col4 = models.CharField(max_length=256, blank=True)
+
+class Record(models.Model):
+    userid = models.IntegerField(blank=True)
+    dataurl = models.CharField(max_length=256)
+
--- app_web/views/query.py
+++ app_web/views/query.py
@@ -3,19 +3,46 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 from app_web.models import Testtable
-import json, time
+from django.core import serializers
+import json, time, os
 
 class Query(APIView):
     permission_classes = ([IsAuthenticated])
     def post(self, request):
-        print('==================')
-        #print(request.body.decode())
         data = request.POST
-        print(data['code[_value]'])
-        print("sleep")
-        #code = data['code']
-        #print(code)
+        code = str(data['code[_value]'])
+        code = code.lower()
+        code = code.replace('testtable','app_web_testtable')
+        if(code.find("limit") == -1):
+            code = code + " limit 100"
+        query_res = Testtable.objects.raw(code)
+        query_res = json.loads(serializers.serialize("json",query_res))
+        query_content_title = []
+        for i in query_res[0]['fields']:
+            query_content_title.append(i)
+        query_content = []
+        num = 1
+        for i in query_res:
+            item = i['fields']
+            item['id'] = num
+            num += 1
+            query_content.append(item)
+        #test = []
+        #for i in list(query_res):
+        #    test.append(list(i))
+        #print(test)
+        #for i in query_res:
+        #    print("======")
+        #    print(i,i.id,i.test)
         query_content = [{'id':1,'col5':'11ds1','col2':'2fsf22','col3':'333'},{'id':2,'col5':'111','col2':'222','col3':'333'},{'id':3,'col5':'111','col2':'222','col3':'333'}]
         query_content_title = ['col5','col2','col3']
         res = {'query_id':4,'query_content':query_content,'query_content_title':query_content_title}
+        folderurl = '/home/zouce/web_mysqlvisualization/app_web/static/file'
+        fileid = len(os.listdir(folderurl)) + 1
+        print(fileid)
+        fileurl = folderurl + '/json' + str(fileid) + '.json'
+        print(fileurl)
+        #file_obj = open(fileurl,'w')
+        #json.dump(res,file_obj)
+        #file_obj.close()
         return Response(res)
